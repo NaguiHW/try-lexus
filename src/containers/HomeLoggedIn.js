214f49c8895123d961aa1c62ac7f000f3b53148c
Kalib Hackin <nagui08@hotmail.com>
2020-03-13 17:58:38 -0500

    working on the side bar, menu done!

--- src/containers/HomeLoggedIn.js
+++ src/containers/HomeLoggedIn.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import lexusLogo from '../img/Lexus.svg';
+import './HomeLoggedIn.scss';
 
 const HomeLoggedIn = ({ logout }) => {
-  
+  const select = e => {
+    const parent = e.target.parentElement;
+    for (let i = 0; i < parent.children.length; i += 1) {
+      parent.children[i].classList.remove('selection');
+    }
+    const item = e.target;
+    item.classList.add('selection');
+  };
+
   return (
     <div className="container-4">
       <div className="side-menu">
@@ -10,18 +19,23 @@ const HomeLoggedIn = ({ logout }) => {
           <img src={lexusLogo} alt="Lexus" />
         </div>
         <div className="menu">
-
+          <nav>
+            <ul>
+              <li onClick={select} role="presentation">SEDANS</li>
+              <li onClick={select} role="presentation">SUVS</li>
+              <li onClick={select} role="presentation">COUPES</li>
+              <li onClick={select} role="presentation">HYBRIDS</li>
+              <li onClick={select} role="presentation">TEST DRIVES</li>
+              <li onClick={select} role="presentation">APPOINTMENTS</li>
+            </ul>
+          </nav>
         </div>
         <div className="social-media">
 
+          <button type="button" onClick={() => logout()}>Log Out</button>
         </div>
       </div>
       <div className="content" />
-      <h1>
-        Home status:
-        (Connected)
-      </h1>
-      <button type="button" onClick={() => logout()}>Log Out</button>
     </div>
   );
 };
