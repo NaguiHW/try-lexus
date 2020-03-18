811104efdbfc63f08e1adb7e6c7963daccf90f7b
Kalib Hackin <nagui08@hotmail.com>
2020-03-18 16:39:18 -0500

    Updates

---
+++ precache-manifest.9ffab8c5c18959ef29c7eb29ee70af8b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9750e4fba3e0a921a7b8ab3919c854aa",
+    "url": "/NaguiHW/try-lexus/tree/release/index.html"
+  },
+  {
+    "revision": "db019d6009ae1dfc28cb",
+    "url": "/NaguiHW/try-lexus/tree/release/static/css/main.eb0f8f7a.chunk.css"
+  },
+  {
+    "revision": "eee79a2b04754ef34ba3",
+    "url": "/NaguiHW/try-lexus/tree/release/static/js/2.4ea090e0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/NaguiHW/try-lexus/tree/release/static/js/2.4ea090e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db019d6009ae1dfc28cb",
+    "url": "/NaguiHW/try-lexus/tree/release/static/js/main.cb60941b.chunk.js"
+  },
+  {
+    "revision": "311b5b7ed042d1eb0d32",
+    "url": "/NaguiHW/try-lexus/tree/release/static/js/runtime-main.4f5a6489.js"
+  },
+  {
+    "revision": "e0893d04f1b803c201925610807ac1d4",
+    "url": "/NaguiHW/try-lexus/tree/release/static/media/Lexus.e0893d04.svg"
+  },
+  {
+    "revision": "5ccb36b696400d646daab329cb0e64da",
+    "url": "/NaguiHW/try-lexus/tree/release/static/media/home.5ccb36b6.jpg"
+  },
+  {
+    "revision": "7cf82b5a73041edf6bd528ac26716c0c",
+    "url": "/NaguiHW/try-lexus/tree/release/static/media/login.7cf82b5a.jpg"
+  },
+  {
+    "revision": "ec9c7c65c6f55c1c2800beb4df1fc264",
+    "url": "/NaguiHW/try-lexus/tree/release/static/media/signup.ec9c7c65.jpg"
+  }
+]);
